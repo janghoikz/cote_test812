3e6916d4dcb61f7f92eb3105a774d05472f3910a
ohne <ghdlr785@gmail.com>
2022-08-13 21:26:01 +0900

    초기 커밋.

---
+++ cote_test812/cote_test812.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string X, string Y) {
+    string answer = "";
+    string a;
+    vector<string> temp;
+    for (int i = 0; i < X.size(); i++)
+    {
+        for (int j = 0; j < Y.size(); j++)
+        {
+            if (X[i] == Y[j])
+            {
+                a = Y[j];
+                temp.push_back(a);
+                Y[j] = 'a';
+                break;
+            }
+        }
+    }
+    if (temp.empty())
+    {
+        return "-1";
+    }
+    // 내림차순 정렬
+    sort(temp.rbegin(), temp.rend());
+
+    if (temp.front() == "0")
+    {
+        return "0";
+    }
+    
+    for (int i = 0; i < temp.size(); i++)
+    {
+        answer += temp[i];
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
